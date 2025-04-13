0c5f2d30fe0b6abe550fef6802a378e92b98f7d0
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-04-13 12:01:07 -0300

    feat: filling new props with data at BarraLateral

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -7,12 +7,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard contador={0} legenda="Pendentes" />
+        <FiltroCard contador={1} legenda="Concluidas" />
+        <FiltroCard contador={2} legenda="Urgentes" />
+        <FiltroCard contador={3} legenda="Importantes" />
+        <FiltroCard contador={4} legenda="Normal" />
+        <FiltroCard ativo contador={5} legenda="Todas" />
       </S.Filtros>
     </div>
   </S.Aside>
