eaaaf614e77b6579f1c9fe58057f2a6590ddd6f5
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-04-29 08:15:57 -0300

    feat: filling value inputs with enums

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../components/FiltroCard'
 
-import * as S from './styles'
 import { RootReducer } from '../../store'
 import { alterarTermo } from '../../store/reducers/filtro'
+
+import * as S from './styles'
+import * as enums from '../../utils/enums/Tarefa'
 
 const BarraLateral = () => {
   const dispatch = useDispatch()
@@ -19,12 +21,37 @@ const BarraLateral = () => {
           onChange={(e) => dispatch(alterarTermo(e.target.value))}
         />
         <S.Filtros>
-          <FiltroCard contador={0} legenda="Pendentes" />
-          <FiltroCard contador={1} legenda="Concluidas" />
-          <FiltroCard contador={2} legenda="Urgentes" />
-          <FiltroCard contador={3} legenda="Importantes" />
-          <FiltroCard contador={4} legenda="Normal" />
-          <FiltroCard ativo contador={5} legenda="Todas" />
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            contador={0}
+            legenda="Pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            contador={1}
+            legenda="Concluidas"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            contador={2}
+            legenda="Urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            contador={3}
+            legenda="Importantes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            contador={4}
+            legenda="Normal"
+          />
+          <FiltroCard criterio="todas" ativo contador={5} legenda="Todas" />
         </S.Filtros>
       </div>
     </S.Aside>
